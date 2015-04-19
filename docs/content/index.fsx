7902c23a0c0e0073cdb0f5fe3765c7766ffa11fe
ashic <ashic@live.com>
2015-04-19 16:38:52 +0100

    updated docs

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -23,6 +23,55 @@ Documentation
 Example
 -------
 
-TODO
+Assuming PersonRegistered implements Started, the following code will trigger both handlers.
+
+```
+var registry = new TypeRegistry<Action<object>>()
+    .Register<PersonRegistered>(x => Console.WriteLine("Person registered."))
+    .Register<Started>(x => Console.WriteLine("Something started."));
+
+var msg = new PersonRegistered(...)
+var handlers = registry.GetValuesFor(msg);
+
+foreach(var h in handlers) h(msg);
+
+```
+
+Assuming PersonRegistered implements Started, the following code will trigger both handlers. Here, Envelope<T> is an interface implemented by
+MessageEnvelope<T>.
+
+```
+var registry = new TypeRegistry<Action<object>>()
+    .Register<Envelope<PersonRegistered>>(x => Console.WriteLine("Person registered."))
+    .Register<Envelope<Started>>(x => Console.WriteLine("Something started."));
+
+var msg = new MessageEnvelope<PersonRegistered>(new PersonRegistered(...))
+var handlers = registry.GetValuesFor(msg);
+
+foreach(var h in handlers) h(msg);
+
+```
+
+Inverse loopuks can be used for type mappings to keys:
+
+```
+
+[TestFixture]
+public class InverseLookupTests
+{
+    readonly TypeRegistry<string> _registry = 
+        new TypeRegistry<string>()
+            .Register<PersonRegisteredEvent>("person-registered")
+            .Register<CriminalRegisteredEvent>("criminal-registered");
+
+    [Test]
+    public void should_get_type_from_value()
+    {
+        Assert.AreEqual(typeof(PersonRegisteredEvent), _registry.GetKeysFor("person-registered").Single().Key);
+        Assert.AreEqual(typeof(CriminalRegisteredEvent), _registry.GetKeysFor("criminal-registered").Single().Key);
+    }
+}
+
+```
 
 *)
